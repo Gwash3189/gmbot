9a04ed82167a28e08557a132cb12c095f8ac1c12
Adam Beck <abeck@zendesk.com>
2016-06-30 20:22:23 +1000

    remove key

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@ require 'bundler'
 Bundler.require
 
 require './gmbot'
+require './key'
 require './models/index'
 
 GmBot.run
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-SLACK_API_TOKEN=xoxb-55186220962-LYsp2qV3zY0fdqMZU433S1zP bundle exec ruby main.rb
+bundle exec ruby main.rb
